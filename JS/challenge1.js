d810e6420b735ce5d7a63f0205f02aad321df483
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-01-20 21:23:56 +0000

    completed challange 1

---
+++ JS/challenge1.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    $('.bottom_button').each(function () {
+        $(this).replaceWith('<button class="bottom_button">' + $(this).text() + '</button>');
+    });
+
+    $(".bottom_button").first().attr('id', 'firstBtn');
+
+    //hides a button as per instuction:
+    $("#firstBtn").click(function() { $(this).hide() });
+
+    
+    //Other fuctions performed on the first button
+
+    // *** hiding paragraph using toggle(); ***
+
+    // $("#firstBtn").click(function() { $(this).siblings("p").toggle("slow") });
+    // $("#firstBtn").click(function() { $(this).siblings("p").toggle("medium") });
+    // $("#firstBtn").click(function() { $(this).siblings("p").toggle("fast") });
+    // $("#firstBtn").click(function() { $(this).siblings("p").toggle(5000) });
+    
+
+    // *** hiding paragraph using slideToggle() ***
+
+    // $("#firstBtn").click(function() { $(this).siblings("p").slideToggle("slow") });
+    // $("#firstBtn").click(function() { $(this).siblings("p").slideToggle("medium") });
+    // $("#firstBtn").click(function() { $(this).siblings("p").slideToggle("fast") });
+    // $("#firstBtn").click(function() { $(this).siblings("p").slideToggle(5000) });
+    
+
+    // *** hiding paragraph using fadeToggle() ***
+
+    // $("#firstBtn").click(function() { $(this).siblings("p").fadeToggle("slow") });
+    // $("#firstBtn").click(function() { $(this).siblings("p").fadeToggle("medium") });
+    // $("#firstBtn").click(function() { $(this).siblings("p").fadeToggle("fast") });
+    // $("#firstBtn").click(function() { $(this).siblings("p").fadeToggle(5000) });
+
+});
---
+++ README.md
@@ -0,0 +1,13 @@
+# jQuery Effects
+
+## Challange 1
+
+### Instructions:
+
+Edit your html file to change all the &lt;a&gt; tags to &lt;button&gt; elements. Don’t forget the closing tag and to remove the href attribute. Make the first button hide when clicked.
+
+![button image](https://s3-eu-west-1.amazonaws.com/codeinstitute/fullstack/05-interactive-front-end-development/Button1.png)
+
+On the same button, try the slow, medium, fast, and a number in milliseconds (you will need to refresh to get the button back).
+
+---
